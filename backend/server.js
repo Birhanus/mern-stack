1e0609b740b97cdf8cdca3181273e97c09910787
birhanus <birhanushimles716@gmail.com>
2022-11-24 23:43:27 +0300

    add midelware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// add body parser for raw json
+app.use(express.json())
+// for url encoding
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/employees', require('./routes/employeeRoutes'))
 
 
