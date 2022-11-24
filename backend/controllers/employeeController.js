5fd83f8776e9bc562762e6343734c5e1ec48069c
birhanus <birhanushimles716@gmail.com>
2022-11-24 23:27:01 +0300

    add comment

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -1,7 +1,43 @@
+
+// @desc Get Employee
+// @route GET api/employee
 const getEmployee = (req, res) => {
     res.status(200).json({welcome: 'Get employee'})
 }
 
+/**
+ * Add Employee
+ * @desc add Employee
+ * @route POST api/employee 
+ * @author  Birhanu Shimles <https://github.com/Birhanus>
+ */ 
+const addEmployee = (req, res) => {
+    console.log(req.body)
+    res.status(200).json({Welcome: 'add  employee'})
+}
+
+/**
+ * update employee data
+ * @desc update Employee
+ * @route PUT api/employee/:id
+ */ 
+const updateEmployee = (req, res) => {
+    res.status(200).json({Welcome: `update employee ${req.params.id}`})
+}
+
+
+/**
+ * Delete Employee
+ * @desc delete Employee
+ * @route DELETE api/employee/:id
+ */
+const deleteEmployee = (req, res) => {
+    res.status(200).json({Welcome: `Delete employee ${req.params.id}`})
+}
+
 module.exports ={
-    getEmployee
+    getEmployee,
+    addEmployee,
+    updateEmployee,
+    deleteEmployee,
 }
