b235c23009d2db513874bdee2bc1d4d46634876f
birhanus <birhanushimles716@gmail.com>
2022-12-04 12:18:48 +0300

    Dashbord

--- frontend/src/pages/Dashbord.tsx
+++ frontend/src/pages/Dashbord.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Dashbord() {
-  return (
-    <div>Dashbord</div>
-  )
+  return <>
+   <section className="heading">
+    <h1>Welcome to  Employee API</h1>
+   </section>
+  </>
 }
 
 export default Dashbord
