5f31abec67fa88edc6a7aecde47599a8d25c4156
birhanus <birhanushimles716@gmail.com>
2022-12-04 12:24:42 +0300

    app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-// import './App.css';
+import React from 'react';
+import Dashbord from './pages/Dashbord';
+import { NavBar } from './components';
+import { EmployeesList, EmployeesInsert, EmployeesUpdate } from './pages';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 function App() {
-  return (
-    <div className="App">
-      <h1>Mern stack Rest API</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact component={Dashbord} />
+                <Route path="/employees/list" exact component={EmployeesList} />
+                <Route path="/employees/create" exact component={EmployeesInsert} />
+                <Route
+                    path="/employees/update/:id"
+                    exact
+                    component={EmployeesUpdate}
+                />
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
+
